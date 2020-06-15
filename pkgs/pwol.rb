3f3518e4054113abe99c613fead4e61bc50f762a
Peter Eriksson <pen@lysator.liu.se>
2020-06-15 16:05:00 +0200

    foo

--- pkgs/pwol.rb
+++ pkgs/pwol.rb
@@ -1,26 +1,20 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Acltool < Formula
-  desc "A tool to manipulate NFSv4/ZFS ACLs (and more) on MacOS, Linux, FreeBSD & Solaris"
+  desc "A tool to send Wake-on-LAN packets to wake-up computers"
   homepage "https://github.com/ptrrkssn/pwol"
   url "https://github.com/ptrrkssn/pwol/archive/v1.5.2.tar.gz"
 
   ## This line must be uncommented and updated with the correct hash value
-  ## The correct value will be displayed when doing 'brew install acltool'
+  ## The correct value will be displayed when doing 'brew install pwol'
   # sha256 "15430b64cb493571f6e46a38482402746bee7ed134c0e99d7976d231cab1c7d5"
 
-  depends_on "readline" => :recommended
-
-  ## Needs Samba's libsmbclient for native SMB support, but we don't force
-  ## it to be installed currently since it brings to much other cruft.
-  #  depends_on "libsmbclient" => :recommended
-  
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 
   test do
-    system "#{bin}/acltool", "lac", "."
+    system "#{bin}/pwol", "-v", "00:01:02:03:04:05"
   end
 end
