78a0f90f6fed21b9ad84c343eeda689b6c55a2c1
Peter Eriksson <pen@lysator.liu.se>
2020-06-15 16:49:18 +0200

    Solaris SVR package support

--- pkgs/pwol.rb
+++ pkgs/pwol.rb
@@ -5,10 +5,10 @@ class Pwol < Formula
   homepage "https://github.com/ptrrkssn/pwol"
   url "https://github.com/ptrrkssn/pwol/archive/v1.5.2.tar.gz"
 
-  ## This line must be uncommented and updated with the correct hash value.
+  ## This line must be uncommented and updated with the correct hash value
   ## The correct value will be displayed when doing 'brew install pwol'
-  # sha256 "f75e6c66d31eb147ea5cf319d851705fdd6d68d7ac25b17f942f4b37a7150fe3"
-  
+  # sha256 "15430b64cb493571f6e46a38482402746bee7ed134c0e99d7976d231cab1c7d5"
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
